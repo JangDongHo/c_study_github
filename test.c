e470c823ee7ecd31dab8b4e1edf4a53f15268ac0
Jang Dong Ho <donghoo18@naver.com>
2019-07-02 17:41:26 +0900

    개수 자동 체크 구문

--- test.c
+++ test.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#define CREAM 500
+#define SAEWOO 700
+#define COKE 400
 
-int main()
+void main()
 {
-	printf("hi");
-	printf("version2");
+	int money;
+	int cr, sa, co;
+	int check = 0;
+
+	printf("현재 당신이 소유하고 있는 금액 : ");
+	scanf("%d", &money);
+
+	for (cr = 1; cr*CREAM < money; cr++)
+	{
+		for (sa = 1; sa*SAEWOO < money; sa++)
+		{
+			for (co = 1; co*COKE < money; co++)
+			{
+				if ((cr * CREAM + sa * SAEWOO + co * COKE) == money)
+				{
+					printf("크림빵 %d개, 새우깡 %d개, 콜 라 %d개\n", cr, sa, co);
+					co = 1;
+					break;
+				}
+			}
+		}
+	}
+
+
 }
