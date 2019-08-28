114bed968e3aa1d85503bc9b45701e62a7f834b3
Jang Dong Ho <donghoo18@naver.com>
2019-08-29 00:44:13 +0900

    가위바위보 게임 version2
    랭킹 오류 존재

--- rework_game.c
+++ rework_game.c
@@ -13,16 +13,22 @@ typedef struct user
 
 int menu();	// 메뉴와 관련된 함수
 void register_login(USER user[10], int* index, int* player);	// 회원가입&로그인 함수
-/*void rate_user(USER user[10], int player, int index);	// 전적, 등수 함수
-void main_game(USER user[10], int player);	// 게임 함수*/
+void rate_user(USER user[10], int player, int index);	// 전적, 등수 함수
+//void main_game(USER user[10], int player);	// 게임 함수
 void file_load(FILE* fp, USER user[10], int* index);	// 파일 불러오기
 void file_save(USER user[10], int index);	// 파일 저장하기
 
 int main()
 {
 	USER user[10];
+	for (int i = 0; i < 10; i++) {      //승,무,패,전을 0으로 초기화합니다.
+		for (int j = 0; j < 4; j++) {
+			user[i].rate[j] = 0;
+		}
+	}
+
 	int index;	// 회원가입 된 유저의 수를 확인하는 변수입니다.
-	int player;	// 로그인된 유저의 인덱스를 확인하는 변수입니다.
+	int player = -1;	// 로그인된 유저의 인덱스를 확인하는 변수입니다. (-1일시, 비로그인 상태)
 
 	// 파일의 존재 유무를 판단하는 구문입니다.
 	FILE* fp;
@@ -32,11 +38,6 @@ int main()
 		// 최초 실행 시 파일을 생성하는 구문입니다.
 		printf("FILE DOWNLOAD (최초 1회)\n");
 		fp = fopen("data.dat", "wb");
-		for (int i = 0; i < 10; i++) {      //승,무,패,전을 0으로 초기화합니다.
-			for (int j = 0; j < 4; j++) {
-				user[i].rate[j] = 0;
-			}
-		}
 		index = -1;	// index 값을 -1로 초기화합니다.
 		printf("[TEST] 현재 인덱스값: %d\n", index);
 	}
@@ -52,8 +53,8 @@ int main()
 		switch ((menu()))
 		{
 		case 1: {register_login(user, &index, &player); break; }
-				/*case 2: {game_user(user, player); break; }
-				case 3: {rate_user(user, player, index); break; }*/
+		//case 2: {game_user(user, player); break; }
+		case 3: {rate_user(user, player, index); break; }
 		case 4: {file_save(user, index); return 0; }
 		}
 	}
@@ -133,6 +134,75 @@ void register_login(USER user[10], int* index, int* player)
 				}
 				else break;
 			}
+		}
+		else break;
+	}
+}
+
+void rate_user(USER user[10], int player, int index)
+{
+	int select;
+	while (1)
+	{
+		// 비로그인 일시 반복문을 탈출합니다.
+		if (player == -1)
+		{
+			printf("먼저 로그인을 해주세요.\n");
+			break;
+		}
+		printf("■■■■■■■■■■■■■■■■■■■\n");
+		printf("1. 전적&등수 확인\n2. 메인메뉴\n");
+		printf("■■■■■■■■■■■■■■■■■■■\n");
+		scanf("%d", &select);
+		// 전적&등수 구문입니다.
+		if (select == 1)
+		{
+			//등수를 계산하는 구문입니다.
+			int win_box[10];	// 첫번째부터 승리 횟수를 가져옵니다.
+			int index_box[10];	// 인덱스 저장용 배열입니다.
+			
+			// 유저수 만큼 인덱스를 0부터 오름차순으로 배열합니다.
+			for (int i = 0; i <= index; i++)
+				index_box[i] = i;
+
+			int temp, temp2;	// 교환하기 위한 임시 변수입니다.
+
+			// 인덱스순으로 승리 횟수를 배열합니다.
+			for (int i = 0; i <= index; i++)
+							win_box[i] = user[i].rate[0];
+			
+			// 인덱스 순으로 승리 횟수를 내림차순 합니다.
+			// 그리고 인덱스 저장용 배열도 같이 바꿔줍니다.
+
+			// 두개씩 비교해서 내림차순을 하는 방법을 이용했습니다.
+			for (int i = 0; i <= index - 1; i++)
+			{
+				for (int j = i + 1; j <= index; j++)
+				{
+					if (win_box[i] < win_box[j])
+					{
+						// 승리횟수 배열을 내림차순합니다.
+						temp = win_box[j];
+						win_box[j] = win_box[i];
+						win_box[i] = temp;
+
+						// 출력을 위해 인덱스 배열도 그에 따라 같이 재배열합니다.
+						temp2 = index_box[j];
+						index_box[j] = index_box[i];
+						index_box[i] = temp2;
+					}
+				}
+			}
+			// 전적을 출력합니다.
+			printf("■■■■■■■■■■■■■■■■■■■\n");
+			printf("【 %s 님의 전적 】승: %d / 패: %d / 무: %d / 전: %d\n", user[player].name, user[player].rate[0], user[player].rate[1], user[player].rate[2], user[player].rate[3]);
+			printf("■■■■■■■■■■■■■■■■■■■\n");
+			// 순위를 출력합니다.
+			for (int i = 0; i <= index; i++)
+			{
+				printf("%2d.【 %s 】 승: %d / 패: %d / 무: %d / 전: %d\n", i + 1, user[index_box[i]].name, user[index_box[i]].rate[0], user[index_box[i]].rate[1], user[index_box[i]].rate[2], user[index_box[i]].rate[3]);
+			}
+			printf("■■■■■■■■■■■■■■■■■■■\n");
 		}
 		else break;
 	}
